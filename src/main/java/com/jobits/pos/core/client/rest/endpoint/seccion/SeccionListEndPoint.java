bf064e67dbcc26d4998a45022a6be5281c3cbf5a
Erik <erikquesada23@gmail.com>
2021-06-11 17:42:57 -0400

    endpoint de paquete seccion implementado

--- src/main/java/com/jobits/pos/core/client/rest/endpoint/seccion/SeccionListEndPoint.java
+++ src/main/java/com/jobits/pos/core/client/rest/endpoint/seccion/SeccionListEndPoint.java
@@ -7,12 +7,19 @@ package com.jobits.pos.core.client.rest.endpoint.seccion;
 
 import com.jobits.pos.controller.seccion.SeccionListService;
 import com.jobits.pos.core.client.rest.assembler.SeccionModelAssembler;
+import com.jobits.pos.core.domain.models.Mesa;
 import com.jobits.pos.core.domain.models.Seccion;
 import com.jobits.pos.core.module.PosCoreModule;
-import com.root101.clean.core.app.usecase.CRUDUseCase;
 import org.jobits.pos.client.rest.assembler.CrudModelAssembler;
 import org.jobits.pos.client.rest.endpoint.CrudRestServiceTemplate;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -23,16 +30,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/seccion_list")
 public class SeccionListEndPoint extends CrudRestServiceTemplate<Seccion> {
 
+    public static final String FIND_SECCION_BY_MESA_PATH = "/find_secciones_by_mesa";
+    public static final RequestMethod FIND_SECCIONES_BY_MESA_METHOD = RequestMethod.GET;
     SeccionModelAssembler seccionAssembler = new SeccionModelAssembler();
 
     @Override
-    public CRUDUseCase<Seccion> getUc() {
+    public SeccionListService getUc() {
         return PosCoreModule.getInstance().getImplementation(SeccionListService.class);
     }
 
     @Override
     public CrudModelAssembler<Seccion> getAssembler() {
         return seccionAssembler;
+    }
+
+    @GetMapping(FIND_SECCION_BY_MESA_PATH)
+    public CollectionModel<EntityModel<Seccion>> findSeccionesByMesa(@RequestBody Mesa mesa) {
+        CollectionModel<EntityModel<Seccion>> entityModel
+                = seccionAssembler.toCollectionModel(getUc().findSeccionesByMesa(mesa));
+        entityModel.add(linkTo(methodOn(SeccionListEndPoint.class).findSeccionesByMesa(mesa)).withRel("find_secciones_by_mesa"));
+        return entityModel;
     }
 
 }
