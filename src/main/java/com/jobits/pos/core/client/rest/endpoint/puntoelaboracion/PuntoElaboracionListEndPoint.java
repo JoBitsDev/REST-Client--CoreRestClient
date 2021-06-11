7601f6ef6d844ab30f096562004090cec51862d3
Erik <erikquesada23@gmail.com>
2021-06-11 17:42:41 -0400

    endpoints de paquete cocina implementado

--- src/main/java/com/jobits/pos/core/client/rest/endpoint/puntoelaboracion/PuntoElaboracionListEndPoint.java
+++ src/main/java/com/jobits/pos/core/client/rest/endpoint/puntoelaboracion/PuntoElaboracionListEndPoint.java
@@ -7,20 +7,19 @@ package com.jobits.pos.core.client.rest.endpoint.puntoelaboracion;
 
 import com.jobits.pos.controller.puntoelaboracion.PuntoElaboracionListService;
 import com.jobits.pos.core.client.rest.assembler.PuntoElaboracionModelAssembler;
-import com.jobits.pos.core.client.rest.endpoint.almacen.AlmacenManageEndPoint;
 import com.jobits.pos.core.domain.models.Cocina;
 import com.jobits.pos.core.module.PosCoreModule;
-import com.root101.clean.core.app.usecase.CRUDUseCase;
 import org.jobits.pos.client.rest.assembler.CrudModelAssembler;
 import org.jobits.pos.client.rest.endpoint.CrudRestServiceTemplate;
-import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  *
@@ -29,6 +28,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(path = "/punto_elaboracion_list")
 public class PuntoElaboracionListEndPoint extends CrudRestServiceTemplate<Cocina> {
+
+    public static final String DESTROY_IN_CASCADE_PATH = "/destroy_in_cascade";
+    public static final RequestMethod DESTROY_IN_CASCADE_METHOD = RequestMethod.DELETE;
+
+    public static final String EDIT_COCINA_PATH = "/edit_cocina";
+    public static final RequestMethod EDIT_COCINA_METHOD = RequestMethod.PUT;
 
     PuntoElaboracionModelAssembler puntoElaboracionAssembler = new PuntoElaboracionModelAssembler();
 
@@ -42,4 +47,17 @@ public class PuntoElaboracionListEndPoint extends CrudRestServiceTemplate<Cocina
         return puntoElaboracionAssembler;
     }
 
+    @DeleteMapping(DESTROY_IN_CASCADE_PATH)
+    public EntityModel<Cocina> destroyInCascade(@RequestBody Cocina cocina) {
+        EntityModel<Cocina> entityModel = puntoElaboracionAssembler.toModel(getUc().destroyInCascade(cocina));
+        entityModel.add(linkTo(methodOn(PuntoElaboracionListEndPoint.class).destroyInCascade(cocina)).withRel("destroy_in_cascade"));
+        return entityModel;
+    }
+
+    @PutMapping(EDIT_COCINA_PATH)
+    public EntityModel<Cocina> edit(@RequestBody Cocina t, @RequestParam String name) {
+        EntityModel<Cocina> entityModel = puntoElaboracionAssembler.toModel(getUc().edit(t, name));
+        entityModel.add(linkTo(methodOn(PuntoElaboracionListEndPoint.class).edit(t, name)).withRel("edit_cocina"));
+        return entityModel;
+    }
 }
