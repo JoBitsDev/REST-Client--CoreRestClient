aaa9144fc4d0ceb77c2bbbaa64d41bc3d5f89142
Jorge Hurtado <jjhurtado796@gmail.com>
2021-11-22 09:32:45 -0500

    Fix: punto elaboracion endpoint

--- src/main/java/com/jobits/pos/core/client/rest/endpoint/puntoelaboracion/PuntoElaboracionListEndPoint.java
+++ src/main/java/com/jobits/pos/core/client/rest/endpoint/puntoelaboracion/PuntoElaboracionListEndPoint.java
@@ -5,59 +5,46 @@
  */
 package com.jobits.pos.core.client.rest.endpoint.puntoelaboracion;
 
-import com.jobits.pos.controller.puntoelaboracion.PuntoElaboracionListService;
-import com.jobits.pos.core.client.rest.assembler.PuntoElaboracionModelAssembler;
 import com.jobits.pos.core.domain.models.Cocina;
 import com.jobits.pos.core.module.PosCoreModule;
-import org.jobits.pos.client.rest.assembler.CrudModelAssembler;
-import org.jobits.pos.client.rest.endpoint.CrudRestServiceTemplate;
-import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import com.jobits.pos.controller.puntoelaboracion.PuntoElaboracionService;
+import org.jobits.pos.client.rest.endpoint.CrudRestEndPointTemplate;
+import org.springframework.web.bind.annotation.PathVariable;
 
 /**
  *
  * @author Home
  */
 @RestController
-@RequestMapping(path = "/punto_elaboracion_list")
-public class PuntoElaboracionListEndPoint extends CrudRestServiceTemplate<Cocina> {
+@RequestMapping(path = "pos/punto-elaboracion")
+public class PuntoElaboracionListEndPoint extends CrudRestEndPointTemplate<Cocina, PuntoElaboracionService>
+        implements PuntoElaboracionService {
 
-    public static final String DESTROY_IN_CASCADE_PATH = "/destroy_in_cascade";
+    public static final String DESTROY_IN_CASCADE_PATH = "/destroy-cascade/{idCocina}";
     public static final RequestMethod DESTROY_IN_CASCADE_METHOD = RequestMethod.DELETE;
 
-    public static final String EDIT_COCINA_PATH = "/edit_cocina";
+    public static final String EDIT_COCINA_PATH = "/{id}/edit-name/{newName}";
     public static final RequestMethod EDIT_COCINA_METHOD = RequestMethod.PUT;
 
-    PuntoElaboracionModelAssembler puntoElaboracionAssembler = new PuntoElaboracionModelAssembler();
-
     @Override
-    public PuntoElaboracionListService getUc() {
-        return PosCoreModule.getInstance().getImplementation(PuntoElaboracionListService.class);
-    }
-
-    @Override
-    public CrudModelAssembler<Cocina> getAssembler() {
-        return puntoElaboracionAssembler;
+    public PuntoElaboracionService getUc() {
+        return PosCoreModule.getInstance().getImplementation(PuntoElaboracionService.class);
     }
 
     @DeleteMapping(DESTROY_IN_CASCADE_PATH)
-    public EntityModel<Cocina> destroyInCascade(@RequestBody Cocina cocina) {
-        EntityModel<Cocina> entityModel = puntoElaboracionAssembler.toModel(getUc().destroyInCascade(cocina));
-        entityModel.add(linkTo(methodOn(PuntoElaboracionListEndPoint.class).destroyInCascade(cocina)).withRel("destroy_in_cascade"));
-        return entityModel;
+    @Override
+    public Cocina destroyInCascade(@PathVariable("idCocina") String nombreCocina) {
+        return getUc().destroyInCascade(nombreCocina);
     }
 
     @PutMapping(EDIT_COCINA_PATH)
-    public EntityModel<Cocina> edit(@RequestBody Cocina t, @RequestParam String name) {
-        EntityModel<Cocina> entityModel = puntoElaboracionAssembler.toModel(getUc().edit(t, name));
-        entityModel.add(linkTo(methodOn(PuntoElaboracionListEndPoint.class).edit(t, name)).withRel("edit_cocina"));
-        return entityModel;
+    @Override
+    public Cocina edit(@PathVariable("id") String codCocina, @PathVariable("newName") String newName) {
+        return getUc().edit(codCocina, newName);
     }
 }
