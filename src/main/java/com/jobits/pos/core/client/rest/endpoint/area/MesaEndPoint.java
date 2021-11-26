aade95b0c64321f0644d24ac49b05b9ef2a0955e
Jorge Hurtado <jjhurtado796@gmail.com>
2021-11-26 05:58:16 -0500

    Puesto Trabajo BETA

--- src/main/java/com/jobits/pos/core/client/rest/endpoint/area/MesaEndPoint.java
+++ src/main/java/com/jobits/pos/core/client/rest/endpoint/area/MesaEndPoint.java
@@ -8,7 +8,14 @@ package com.jobits.pos.core.client.rest.endpoint.area;
 import com.jobits.pos.controller.areaventa.MesaService;
 import com.jobits.pos.core.domain.models.Mesa;
 import com.jobits.pos.core.module.PosCoreModule;
-import org.jobits.pos.client.rest.endpoint.CrudRestEndPointTemplate;
+import com.root101.clean.core.app.usecase.AbstractUseCaseImpl;
+import java.util.List;
+import org.jobits.pos.client.rest.endpoint.UrlTemplate;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,14 +25,42 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping(path = "pos/mesa")
-public class MesaEndPoint extends CrudRestEndPointTemplate<Mesa,MesaService> implements MesaService{
+public class MesaEndPoint extends AbstractUseCaseImpl implements MesaService {
 
+    public MesaEndPoint() {
+    }
 
-    @Override
     public MesaService getUc() {
         return PosCoreModule.getInstance().getImplementation(MesaService.class);
     }
 
- 
+    @Override
+    @PutMapping(UrlTemplate.EDIT_PATH)
+    public Mesa edit(@RequestBody Mesa t) throws RuntimeException {
+        return getUc().edit(t);
+    }
+
+    @Override
+    public Mesa destroy(Mesa t) throws RuntimeException {
+        throw new UnsupportedOperationException("En desarrollo");
+    }
+
+    @Override
+    @DeleteMapping(UrlTemplate.DESTROY_ID_PATH)
+    public Mesa destroyById(@PathVariable("id") Object o) throws RuntimeException {
+        return getUc().destroyById(o);
+    }
+
+    @Override
+    @GetMapping(UrlTemplate.FIND_BY_PATH)
+    public Mesa findBy(@PathVariable("id") Object o) throws RuntimeException {
+        return getUc().findBy(o);
+    }
+
+    @Override
+    @GetMapping(UrlTemplate.FIND_ALL_PATH)
+    public List<Mesa> findAll() throws RuntimeException {
+        return getUc().findAll();
+    }
 
 }
--- src/main/java/com/jobits/pos/core/client/rest/endpoint/trabajador/PuestoTrabajoEndPoint.java
+++ src/main/java/com/jobits/pos/core/client/rest/endpoint/trabajador/PuestoTrabajoEndPoint.java
@@ -5,34 +5,25 @@
  */
 package com.jobits.pos.core.client.rest.endpoint.trabajador;
 
-import com.jobits.pos.core.client.rest.assembler.PuestoTrabajoModelAssembler;
 import com.jobits.pos.core.domain.models.PuestoTrabajo;
 import com.jobits.pos.core.module.PosCoreModule;
-import com.root101.clean.core.app.usecase.CRUDUseCase;
-import org.jobits.pos.client.rest.assembler.CrudModelAssembler;
-import org.jobits.pos.client.rest.endpoint.CrudRestServiceTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.jobits.pos.controller.trabajadores.PuestoTrabajoUseCase;
+import org.jobits.pos.client.rest.endpoint.CrudRestEndPointTemplate;
 
 /**
  *
  * @author Home
  */
 @RestController
-@RequestMapping(path = "/puesto_trabajo_detail")
-public class PuestoTrabajoDetailEndPoint extends CrudRestServiceTemplate<PuestoTrabajo> {
-
-    PuestoTrabajoModelAssembler puestoTrabajoAssembler = new PuestoTrabajoModelAssembler();
+@RequestMapping(path = "pos/puesto-trabajo")
+public class PuestoTrabajoEndPoint extends CrudRestEndPointTemplate<PuestoTrabajo, PuestoTrabajoUseCase>
+        implements PuestoTrabajoUseCase {
 
     @Override
-    public CRUDUseCase<PuestoTrabajo> getUc() {
+    public PuestoTrabajoUseCase getUc() {
         return PosCoreModule.getInstance().getImplementation(PuestoTrabajoUseCase.class);
-    }
-
-    @Override
-    public CrudModelAssembler<PuestoTrabajo> getAssembler() {
-        return puestoTrabajoAssembler;
     }
 
 }
