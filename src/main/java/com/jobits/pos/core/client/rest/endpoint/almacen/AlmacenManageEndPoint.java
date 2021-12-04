b7c24a153af2746e500a6552b8ec08c2ebd55827
Jorge Hurtado <jjhurtado796@gmail.com>
2021-12-04 13:44:10 -0500

    Clear almacen endpoint

--- src/main/java/com/jobits/pos/core/client/rest/endpoint/almacen/AlmacenManageEndPoint.java
+++ src/main/java/com/jobits/pos/core/client/rest/endpoint/almacen/AlmacenManageEndPoint.java
@@ -6,11 +6,8 @@
 package com.jobits.pos.core.client.rest.endpoint.almacen;
 
 import com.jobits.pos.controller.almacen.AlmacenManageService;
-import com.jobits.pos.core.client.rest.assembler.AlmacenModelAssembler;
-import com.jobits.pos.core.client.rest.assembler.InsumoAlmacenModelAssembler;
 import com.jobits.pos.core.domain.TransaccionSimple;
 import com.jobits.pos.core.domain.models.Almacen;
-import com.jobits.pos.core.domain.models.Insumo;
 import com.jobits.pos.core.domain.models.InsumoAlmacen;
 import com.jobits.pos.core.domain.models.Transaccion;
 import com.jobits.pos.core.domain.models.TransaccionEntrada;
@@ -18,23 +15,13 @@ import com.jobits.pos.core.domain.models.TransaccionMerma;
 import com.jobits.pos.core.domain.models.TransaccionSalida;
 import com.jobits.pos.core.domain.models.TransaccionTransformacion;
 import com.jobits.pos.core.domain.models.TransaccionTraspaso;
-import com.jobits.pos.core.module.PosCoreModule;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import org.jobits.pos.client.rest.assembler.CrudModelAssembler;
-import org.jobits.pos.client.rest.endpoint.CrudRestServiceTemplate;
-import org.springframework.hateoas.CollectionModel;
-import org.springframework.hateoas.EntityModel;
+import org.jobits.pos.client.rest.endpoint.CrudRestEndPointTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  *
@@ -42,7 +29,8 @@ import org.springframework.web.bind.annotation.RequestParam;
  */
 @RestController
 @RequestMapping(path = "/almacen_manage")
-public class AlmacenManageEndPoint extends CrudRestServiceTemplate<Almacen> {
+public class AlmacenManageEndPoint extends CrudRestEndPointTemplate<Almacen,AlmacenManageService> 
+        implements AlmacenManageService{
 
     public static final String INSUMO_ALMACEN_LIST_PATH = "/insumo_almacen_list";
     public static final RequestMethod INSUMO_ALMACEN_LIST_METHOD = RequestMethod.GET;
@@ -104,149 +92,114 @@ public class AlmacenManageEndPoint extends CrudRestServiceTemplate<Almacen> {
     public static final String CREAR_OPERACION_TRASPASO_PATH = "/crear_operacion_traspaso";
     public static final RequestMethod CREAR_OPERACION_TRASPASO_METHOD = RequestMethod.POST;
 
-    AlmacenModelAssembler almacenAssembler = new AlmacenModelAssembler();
-    InsumoAlmacenModelAssembler insumoAlmacenAssembler = new InsumoAlmacenModelAssembler();
-
     @Override
     public AlmacenManageService getUc() {
-        return PosCoreModule.getInstance().getImplementation(AlmacenManageService.class);
+        throw new UnsupportedOperationException(); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public CrudModelAssembler<Almacen> getAssembler() {
-        return almacenAssembler;
+    public void resetAlmacen(String codAlmacen) {
+        throw new UnsupportedOperationException(); //To change body of generated methods, choose Tools | Templates.
     }
 
-    @PostMapping(CREAR_OPERACION_ENTRADA_PATH)
-    public boolean crearOperacionEntrada(@RequestBody ArrayList<TransaccionSimple> al,
-            @RequestParam String string, @RequestParam Date date, @RequestBody Almacen almcn) {
-        getUc().crearOperacionEntrada(al, string, date, almcn);
-        return true;
+    @Override
+    public void crearOperacionEntrada(ArrayList<TransaccionSimple> transacciones, String recibo, Date fechaFactura, String codAlmacen) {
+        throw new UnsupportedOperationException(); //To change body of generated methods, choose Tools | Templates.
     }
 
-    @PostMapping(CREAR_OPERACION_REBAJA_PATH)
-    public boolean crearOperacionRebaja(@RequestBody ArrayList<TransaccionSimple> al,
-            @RequestParam String string, @RequestParam Date date, @RequestBody Almacen almcn) {
-        getUc().crearOperacionRebaja(al, string, date, almcn);
-        return true;
+    @Override
+    public void crearOperacionRebaja(ArrayList<TransaccionSimple> transacciones, String recibo, Date fechaFactura, String codAlmacen) {
+        throw new UnsupportedOperationException(); //To change body of generated methods, choose Tools | Templates.
     }
 
-    @PostMapping(CREAR_OPERACION_SALIDA_PATH)
-    public boolean crearOperacionSalida(@RequestBody ArrayList<TransaccionSimple> al,
-            @RequestParam String string, @RequestParam Date date,
-            @RequestParam Integer intgr, @RequestParam Almacen almcn) {
-        getUc().crearOperacionSalida(al, string, date, intgr, almcn);
-        return true;
+    @Override
+    public void crearOperacionSalida(ArrayList<TransaccionSimple> transacciones, String recibo, Date fechaFactura, Integer codVenta, String codAlmacen) {
+        throw new UnsupportedOperationException(); //To change body of generated methods, choose Tools | Templates.
     }
 
-    @PostMapping(CREAR_OPERACION_TRASPASO_PATH)
-    public boolean crearOperacionTraspaso(@RequestBody ArrayList<TransaccionSimple> al,
-            @RequestParam String string, @RequestBody Date date, @RequestBody Almacen almcn) {
-        getUc().crearOperacionTraspaso(al, string, date, almcn);
-        return true;
+    @Override
+    public void crearOperacionTraspaso(ArrayList<TransaccionSimple> transacciones, String recibo, Date fechaFactura, String codAlmacen) {
+        throw new UnsupportedOperationException(); //To change body of generated methods, choose Tools | Templates.
     }
 
-    @PutMapping(DAR_ENTRADA_A_INSUMO_PATH)
-    public boolean darEntradaAInsumo(@RequestBody TransaccionEntrada te, @RequestBody Almacen almcn) {
-        getUc().darEntradaAInsumo(te, almcn);
-        return true;
+    @Override
+    public void darEntradaAInsumo(TransaccionEntrada x, String codAlmacen) {
+        throw new UnsupportedOperationException(); //To change body of generated methods, choose Tools | Templates.
     }
 
-    @PutMapping(DAR_MERMA_A_INSUMO_PATH)
-    public boolean darMermaInsumo(@RequestBody TransaccionMerma tm, @RequestBody Almacen almcn) {
-        getUc().darMermaInsumo(tm, almcn);
-        return true;
+    @Override
+    public void darMermaInsumo(TransaccionMerma x, String codAlmacen) {
+        throw new UnsupportedOperationException(); //To change body of generated methods, choose Tools | Templates.
     }
 
-    @PutMapping(DAR_SALIDA_A_INSUMO_PATH)
-    public boolean darSalidaAInsumo(@RequestBody TransaccionSalida ts,
-            @RequestParam int i, @RequestBody Almacen almcn) {
-        getUc().darSalidaAInsumo(ts, i, almcn);
-        return true;
+    @Override
+    public void darSalidaAInsumo(TransaccionSalida x, int idVenta, String codAlmacen) {
+        throw new UnsupportedOperationException(); //To change body of generated methods, choose Tools | Templates.
     }
 
-    @PutMapping(DAR_TRANSFORMACION_A_INSUMO_PATH)
-    public boolean darTransformacionAInsumo(@RequestBody Transaccion trnscn,
-            @RequestBody Almacen almcn, @RequestBody Almacen almcn1) {
-        getUc().darTransformacionAInsumo(trnscn, almcn, almcn1);
-        return true;
+    @Override
+    public void darTransformacionAInsumo(Transaccion t, String codAlmacenDestino, String codAlmacenOrigen) {
+        throw new UnsupportedOperationException(); //To change body of generated methods, choose Tools | Templates.
     }
 
-    @PutMapping(DAR_TRASPASO_A_INSUMO_PATH)
-    public boolean darTraspasoInsumo(@RequestBody TransaccionTraspaso tt,
-            @RequestBody Almacen almcn) {
-        getUc().darTraspasoInsumo(tt, almcn);
-        return true;
+    @Override
+    public void darTraspasoInsumo(TransaccionTraspaso x, String codAlmacen) {
+        throw new UnsupportedOperationException(); //To change body of generated methods, choose Tools | Templates.
     }
 
-    @PutMapping(AGREGAR_INSUMO_ALMACEN_PATH)
-    public boolean agregarInsumoAlmacen(@RequestBody Insumo insumo,
-            @RequestBody Almacen almcn) {
-        getUc().agregarInsumoAlmacen(insumo, almcn);
-        return true;
+    @Override
+    public void agregarInsumoAlmacen(String codInsumo, String codAlmacen) {
+        throw new UnsupportedOperationException(); //To change body of generated methods, choose Tools | Templates.
     }
 
-    @PostMapping(CREAR_TRANSFORMACION_PATH)
-    public boolean crearTransformacion(@RequestBody InsumoAlmacen ia,
-            @RequestParam float f, @RequestBody List<TransaccionTransformacion> list,
-            @RequestBody Almacen almcn) {
-        getUc().crearTransformacion(ia, f, list, almcn);
-        return true;
+    @Override
+    public void crearTransformacion(InsumoAlmacen selected, float cantidad, List<TransaccionTransformacion> items, String codAlmacen) {
+        throw new UnsupportedOperationException(); //To change body of generated methods, choose Tools | Templates.
     }
 
-    @PutMapping(SET_CENTRO_ELABORACION_PATH)
-    public boolean setCentroElaboracion(@RequestParam boolean bln, @RequestBody Almacen almcn) {
-        getUc().setCentroElaboracion(bln, almcn);
-        return true;
+    @Override
+    public void setCentroElaboracion(boolean selected, String codAlmacen) {
+        throw new UnsupportedOperationException(); //To change body of generated methods, choose Tools | Templates.
     }
 
-    @PostMapping(IMPRIMIR_RESUMEN_COMPRAS_PATH)
-    public boolean imprimirReporteParaCompras(@RequestBody Almacen almcn, @RequestParam int i) {
-        getUc().imprimirReporteParaCompras(almcn, i);
-        return true;
+    @Override
+    public void imprimirReporteParaCompras(String codAlmacen, int selection) {
+        throw new UnsupportedOperationException(); //To change body of generated methods, choose Tools | Templates.
     }
 
-    @PostMapping(IMPRIMIR_RESUMEN_ALMACEN_PATH)
-    public boolean imprimirResumenAlmacen(@RequestBody Almacen almcn) {
-        getUc().imprimirResumenAlmacen(almcn);
-        return true;
+    @Override
+    public void imprimirResumenAlmacen(String codAlmacen) {
+        throw new UnsupportedOperationException(); //To change body of generated methods, choose Tools | Templates.
     }
 
-    @PutMapping(REMOVE_INSUMO_FROM_STRORAGE_PATH)
-    public boolean removeInsumoFromStorage(@RequestBody InsumoAlmacen ia, @RequestBody Almacen almcn) {
-        getUc().removeInsumoFromStorage(ia, almcn);
-        return true;
+    @Override
+    public void removeInsumoFromStorage(InsumoAlmacen insumoAlmacen, String codAlmacen) {
+        throw new UnsupportedOperationException(); //To change body of generated methods, choose Tools | Templates.
     }
 
-    @PutMapping(UPDATE_VALOR_TOTAL_ALMACEN_PATH)
-    public boolean updateValorTotalAlmacen(@RequestBody Almacen almcn) {
-        getUc().updateValorTotalAlmacen(almcn);
-        return true;
+    @Override
+    public void updateValorTotalAlmacen(String codAlmacen) {
+        throw new UnsupportedOperationException(); //To change body of generated methods, choose Tools | Templates.
     }
 
-    @PostMapping(DAR_ENTRADA_IPV_PATH)
-    public boolean darEntradaIPV(@RequestBody Almacen almcn, @RequestBody Insumo insumo, @RequestParam float f) {
-        getUc().darEntradaIPV(almcn, insumo, f);
-        return true;
+    @Override
+    public void darEntradaIPV(String codAlmacen, String codInsumo, float cantidad) {
+        throw new UnsupportedOperationException(); //To change body of generated methods, choose Tools | Templates.
     }
 
-    @GetMapping(BULK_INSUMO_PATH)
-    public boolean bulkImport(@RequestBody List<InsumoAlmacen> list) {
-        return getUc().bulkImport(list);
+    @Override
+    public boolean bulkImport(List<InsumoAlmacen> importList) {
+        throw new UnsupportedOperationException(); //To change body of generated methods, choose Tools | Templates.
     }
 
-    @GetMapping(INSUMO_ALMACEN_LIST_PATH)
-    public CollectionModel<EntityModel<InsumoAlmacen>> getInsumoAlmacenList(@RequestBody Almacen almcn) {
-        CollectionModel<EntityModel<InsumoAlmacen>> entityModel
-                = insumoAlmacenAssembler.toCollectionModel(getUc().getInsumoAlmacenList(almcn));
-        entityModel.add(linkTo(methodOn(AlmacenManageEndPoint.class).getInsumoAlmacenList(almcn)).withRel("insumo_almacen_list"));
-        return entityModel;
+    @Override
+    public List<InsumoAlmacen> getInsumoAlmacenList(String codAlmacen) {
+        throw new UnsupportedOperationException(); //To change body of generated methods, choose Tools | Templates.
     }
 
-    @GetMapping(FIND_INSUMO_PATH)
-    public EntityModel<InsumoAlmacen> findInsumo(@RequestBody Insumo insumo, @RequestBody Almacen almcn) {
-        EntityModel<InsumoAlmacen> entityModel = insumoAlmacenAssembler.toModel(getUc().findInsumo(insumo, almcn));
-        entityModel.add(linkTo(methodOn(AlmacenManageEndPoint.class).findInsumo(insumo, almcn)).withRel("find_insumo"));
-        return entityModel;
+    @Override
+    public InsumoAlmacen findInsumo(String codInsumo, String codAlmacen) {
+        throw new UnsupportedOperationException(); //To change body of generated methods, choose Tools | Templates.
     }
+
 }
