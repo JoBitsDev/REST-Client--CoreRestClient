cd5401e4bc11e91730f6d61df3f7e8e2fcafd4a4
Jorge Hurtado <jjhurtado796@gmail.com>
2022-01-23 20:05:44 -0500

    basic implementation ventaEndPoint

--- src/main/java/com/jobits/pos/core/client/rest/endpoint/venta/VentaEndPoint.java
+++ src/main/java/com/jobits/pos/core/client/rest/endpoint/venta/VentaEndPoint.java
@@ -6,21 +6,15 @@
 package com.jobits.pos.core.client.rest.endpoint.venta;
 
 import com.jobits.pos.controller.venta.VentaDetailService;
-import com.jobits.pos.core.domain.VentaResourcesWrapper;
-import com.jobits.pos.core.domain.models.Area;
-import com.jobits.pos.core.domain.models.Cocina;
-import com.jobits.pos.core.domain.models.IpvRegistro;
-import com.jobits.pos.core.domain.models.Mesa;
 import com.jobits.pos.core.domain.models.Orden;
-import com.jobits.pos.core.domain.models.Personal;
-import com.jobits.pos.core.domain.models.ProductoVenta;
 import com.jobits.pos.core.domain.models.Venta;
 import com.jobits.pos.core.module.PosCoreModule;
 import java.io.File;
 import java.time.LocalDate;
-import java.util.Date;
 import java.util.List;
 import org.jobits.pos.client.rest.endpoint.CrudRestEndPointTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,119 +23,119 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Home
  */
 @RestController
-@RequestMapping(path = "/venta")
+@RequestMapping(path = "pos/venta")
 public class VentaEndPoint extends CrudRestEndPointTemplate<Venta, VentaDetailService>
         implements VentaDetailService {
 
     @Override
     public VentaDetailService getUc() {
-        throw new UnsupportedOperationException(); //To change body of generated methods, choose Tools | Templates.
+        return PosCoreModule.getInstance().getImplementation(VentaDetailService.class);
     }
 
     @Override
     public List<Venta> getVentasDeFecha(LocalDate date) {
-        throw new UnsupportedOperationException(); //To change body of generated methods, choose Tools | Templates.
+        return getUc().getVentasDeFecha(date);
     }
 
     @Override
     public List<Venta> getVentasDeFecha(int idVenta) {
-        throw new UnsupportedOperationException(); //To change body of generated methods, choose Tools | Templates.
+        return getUc().getVentasDeFecha(idVenta);
     }
 
     @Override
     public Venta cambiarTurno(int idTurnoATerminar) {
-        throw new UnsupportedOperationException(); //To change body of generated methods, choose Tools | Templates.
+        return getUc().cambiarTurno(idTurnoATerminar);
     }
 
     @Override
     public boolean canOpenNuevoTurno(LocalDate fecha) {
-        throw new UnsupportedOperationException(); //To change body of generated methods, choose Tools | Templates.
+        return getUc().canOpenNuevoTurno(fecha);
     }
 
     @Override
     public boolean canOpenNuevoTurno(int idVenta) {
-        throw new UnsupportedOperationException(); //To change body of generated methods, choose Tools | Templates.
+        return getUc().canOpenNuevoTurno(idVenta);
     }
 
     @Override
     public boolean canReabrirVenta(int codVenta) {
-        throw new UnsupportedOperationException(); //To change body of generated methods, choose Tools | Templates.
+        return getUc().canReabrirVenta(codVenta);
     }
 
     @Override
     public Orden createNewOrden(int codVenta, String codMesa) {
-        throw new UnsupportedOperationException(); //To change body of generated methods, choose Tools | Templates.
+        return getUc().createNewOrden(codVenta, codMesa);
     }
 
     @Override
     public float getAutorizosTotalDelProducto(String codProductoVenta, int codVenta) {
-        throw new UnsupportedOperationException(); //To change body of generated methods, choose Tools | Templates.
+        return getUc().getAutorizosTotalDelProducto(codProductoVenta, codVenta);
     }
 
     @Override
     public Float getGastoTotalDeInsumo(String codCocina, String codInsumo, int codVenta) {
-        throw new UnsupportedOperationException(); //To change body of generated methods, choose Tools | Templates.
+        return getUc().getGastoTotalDeInsumo(codCocina, codInsumo, codVenta);
     }
 
     @Override
     public List<Orden> getOrdenesActivas(int codVenta) {
-        throw new UnsupportedOperationException(); //To change body of generated methods, choose Tools | Templates.
+        return getUc().getOrdenesActivas(codVenta);
     }
 
     @Override
     public Float getPagoTrabajador(String codPersonal, int codVenta, int dividirEntre) {
-        throw new UnsupportedOperationException(); //To change body of generated methods, choose Tools | Templates.
+        return getUc().getPagoTrabajador(codPersonal, codVenta, dividirEntre);
     }
 
     @Override
     public Float getPropinaTrabajador(String codPersonal, int codVenta) {
+        return getUc().getPropinaTrabajador(codPersonal, codVenta);
+    }
+
+    @Override
+    public void importarVenta(File file) {//TODO; implementar
         throw new UnsupportedOperationException(); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public void importarVenta(File file) {
-        throw new UnsupportedOperationException(); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public Venta inicializarVentas(LocalDate fecha, boolean nuevaVenta) {
-        throw new UnsupportedOperationException(); //To change body of generated methods, choose Tools | Templates.
+    @PostMapping("/inicializar")
+    public Venta inicializarVentas(@RequestBody LocalDate fecha) {
+        return getUc().inicializarVentas(fecha);
     }
 
     @Override
     public void printGastosCasa(int codVenta) {
-        throw new UnsupportedOperationException(); //To change body of generated methods, choose Tools | Templates.
+        getUc().printGastosCasa(codVenta);
     }
 
     @Override
     public void printPagoPorVentaPersonal(String codPersonal, int codVenta, boolean printValores) {
-        throw new UnsupportedOperationException(); //To change body of generated methods, choose Tools | Templates.
+        getUc().printPagoPorVentaPersonal(codPersonal, codVenta, printValores);
     }
 
     @Override
     public void printComisionPorcentualResumen(String codMesa, int idVenta) {
-        throw new UnsupportedOperationException(); //To change body of generated methods, choose Tools | Templates.
+        getUc().printComisionPorcentualResumen(codMesa, idVenta);
     }
 
     @Override
     public void printZ(int codVenta) {
-        throw new UnsupportedOperationException(); //To change body of generated methods, choose Tools | Templates.
+        getUc().printZ(codVenta);
     }
 
     @Override
     public void reabrirVentas(int codVenta) {
-        throw new UnsupportedOperationException(); //To change body of generated methods, choose Tools | Templates.
+        getUc().reabrirVentas(codVenta);
     }
 
     @Override
     public void terminarVentas(int codVenta) {
-        throw new UnsupportedOperationException(); //To change body of generated methods, choose Tools | Templates.
+        getUc().terminarVentas(codVenta);
     }
 
     @Override
-    public void terminarYExportar(File file, int codVenta) {
+    public void terminarYExportar(File file, int codVenta) {//TODO: implementar
         throw new UnsupportedOperationException(); //To change body of generated methods, choose Tools | Templates.
     }
-
 
 }
