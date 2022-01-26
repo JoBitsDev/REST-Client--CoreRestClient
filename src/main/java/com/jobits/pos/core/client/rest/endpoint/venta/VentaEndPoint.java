d1b45501626544a0cd8da2c973930936089b48b3
Jorge Hurtado <jjhurtado796@gmail.com>
2022-01-25 20:49:31 -0500

    URL set

--- src/main/java/com/jobits/pos/core/client/rest/endpoint/venta/VentaEndPoint.java
+++ src/main/java/com/jobits/pos/core/client/rest/endpoint/venta/VentaEndPoint.java
@@ -12,8 +12,12 @@ import com.jobits.pos.core.module.PosCoreModule;
 import java.io.File;
 import java.time.LocalDate;
 import java.util.List;
+import javax.websocket.server.PathParam;
 import org.jobits.pos.client.rest.endpoint.CrudRestEndPointTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,68 +37,80 @@ public class VentaEndPoint extends CrudRestEndPointTemplate<Venta, VentaDetailSe
     }
 
     @Override
-    public List<Venta> getVentasDeFecha(LocalDate date) {
+    @GetMapping("list-of-date/{dateISO}")
+    public List<Venta> getVentasDeFecha(@PathVariable("dateISO") LocalDate date) {
         return getUc().getVentasDeFecha(date);
     }
 
     @Override
-    public List<Venta> getVentasDeFecha(int idVenta) {
+    @GetMapping("{id}/list-all")
+    public List<Venta> getVentasDeFecha(@PathVariable("id") int idVenta) {
         return getUc().getVentasDeFecha(idVenta);
     }
 
     @Override
-    public Venta cambiarTurno(int idTurnoATerminar) {
+    @PostMapping("{id}/cambiar-turno")
+    public Venta cambiarTurno(@PathVariable("id") int idTurnoATerminar) {
         return getUc().cambiarTurno(idTurnoATerminar);
     }
 
     @Override
-    public boolean canOpenNuevoTurno(LocalDate fecha) {
+    @GetMapping("can-open-nuevo-turno/{dateISO}")
+    public boolean canOpenNuevoTurno(@PathVariable("dateISO") LocalDate fecha) {
         return getUc().canOpenNuevoTurno(fecha);
     }
 
     @Override
-    public boolean canOpenNuevoTurno(int idVenta) {
+    @GetMapping("{id}/can-open-nuevo-turno")
+    public boolean canOpenNuevoTurno(@PathVariable("id") int idVenta) {
         return getUc().canOpenNuevoTurno(idVenta);
     }
 
     @Override
-    public boolean canReabrirVenta(int codVenta) {
+    @GetMapping("{id}/can-reabrir")
+    public boolean canReabrirVenta(@PathVariable("id") int codVenta) {
         return getUc().canReabrirVenta(codVenta);
     }
 
     @Override
-    public Orden createNewOrden(int codVenta, String codMesa) {
+    @PostMapping("{id}/create-new-orden/{idMesa}")
+    public Orden createNewOrden(@PathVariable("id") int codVenta, @PathVariable("idMesa") String codMesa) {
         return getUc().createNewOrden(codVenta, codMesa);
     }
 
     @Override
+    //  @GetMapping("{id}/get-autorizos-producto")
     public float getAutorizosTotalDelProducto(String codProductoVenta, int codVenta) {
-        return getUc().getAutorizosTotalDelProducto(codProductoVenta, codVenta);
+        throw new UnsupportedOperationException("Operacion no permitida"); //   return getUc().getAutorizosTotalDelProducto(codProductoVenta, codVenta);
     }
 
     @Override
     public Float getGastoTotalDeInsumo(String codCocina, String codInsumo, int codVenta) {
-        return getUc().getGastoTotalDeInsumo(codCocina, codInsumo, codVenta);
+        throw new UnsupportedOperationException("Operacion no permitida"); //   return getUc().getAutorizosTotalDelProducto(codProductoVenta, codVenta);
+
     }
 
     @Override
-    public List<Orden> getOrdenesActivas(int codVenta) {
+    @GetMapping("{id}/ordenes-activas")
+    public List<Orden> getOrdenesActivas(@PathVariable("id") int codVenta) {
         return getUc().getOrdenesActivas(codVenta);
     }
 
     @Override
-    public Float getPagoTrabajador(String codPersonal, int codVenta, int dividirEntre) {
-        return getUc().getPagoTrabajador(codPersonal, codVenta, dividirEntre);
+    @GetMapping("{id}/get-pago-trabajador/{dividirEntre}/{codPersonal}")
+    public Float getPagoTrabajador(@PathVariable("codPersonal") String codPersonal, int codVenta, int dividirEntre) {
+        throw new UnsupportedOperationException("Operacion no permitida"); //   return getUc().getAutorizosTotalDelProducto(codProductoVenta, codVenta);
     }
 
     @Override
     public Float getPropinaTrabajador(String codPersonal, int codVenta) {
-        return getUc().getPropinaTrabajador(codPersonal, codVenta);
+        throw new UnsupportedOperationException("Operacion no permitida"); //   return getUc().getAutorizosTotalDelProducto(codProductoVenta, codVenta);
     }
 
     @Override
+    @PutMapping("importar")
     public void importarVenta(File file) {//TODO; implementar
-        throw new UnsupportedOperationException(); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Operacion deshabilitada temporalmente"); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
@@ -104,38 +120,46 @@ public class VentaEndPoint extends CrudRestEndPointTemplate<Venta, VentaDetailSe
     }
 
     @Override
-    public void printGastosCasa(int codVenta) {
+    @PostMapping("{id}/print/autorizos")
+    public void printGastosCasa(@PathVariable("{id}") int codVenta) {
         getUc().printGastosCasa(codVenta);
     }
 
     @Override
-    public void printPagoPorVentaPersonal(String codPersonal, int codVenta, boolean printValores) {
+    @PostMapping("{id}/print/pago-por-venta/{usuario}")
+    public void printPagoPorVentaPersonal(@PathVariable("usuario") String codPersonal,
+            @PathVariable("id") int codVenta, @PathParam("printValues") boolean printValores) {
         getUc().printPagoPorVentaPersonal(codPersonal, codVenta, printValores);
     }
 
     @Override
-    public void printComisionPorcentualResumen(String codMesa, int idVenta) {
+    @PostMapping("{id}/print/comision-pocentual/{codMesa}")
+    public void printComisionPorcentualResumen(@PathVariable("codMesa") String codMesa, @PathVariable("id") int idVenta) {
         getUc().printComisionPorcentualResumen(codMesa, idVenta);
     }
 
     @Override
-    public void printZ(int codVenta) {
+    @PostMapping("{id}/print/z")
+    public void printZ(@PathVariable("id") int codVenta) {
         getUc().printZ(codVenta);
     }
 
     @Override
-    public void reabrirVentas(int codVenta) {
+    @PostMapping("{id}/reabrir-ventas")
+    public void reabrirVentas(@PathVariable("id") int codVenta) {
         getUc().reabrirVentas(codVenta);
     }
 
     @Override
-    public void terminarVentas(int codVenta) {
+    @PostMapping("{id}/terminar-ventas")
+    public void terminarVentas(@PathParam("id") int codVenta) {
         getUc().terminarVentas(codVenta);
     }
 
     @Override
-    public void terminarYExportar(File file, int codVenta) {//TODO: implementar
-        throw new UnsupportedOperationException(); //To change body of generated methods, choose Tools | Templates.
+    @PostMapping("{id}/terminar-y-exportar")
+    public void terminarYExportar(File file, @PathVariable("id") int codVenta) {//TODO: implementar
+        throw new UnsupportedOperationException("Operacion deshabilitada temporalmente"); //To change body of generated methods, choose Tools | Templates.
     }
 
 }
