9913a0c6e48b52a044548e68f9371758420ea1c0
Jorge Hurtado <jjhurtado796@gmail.com>
2022-08-05 10:31:10 -0400

    FIX resumen de ventas

--- src/main/java/com/jobits/pos/core/client/rest/endpoint/venta/VentaResumenEndPoint.java
+++ src/main/java/com/jobits/pos/core/client/rest/endpoint/venta/VentaResumenEndPoint.java
@@ -15,6 +15,7 @@ import com.jobits.pos.core.domain.models.temporal.ResumenVentaWrapper;
 import com.jobits.pos.core.module.PosCoreModule;
 import java.time.LocalDate;
 import org.jobits.pos.client.rest.endpoint.DefaultEndpoint;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,7 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Home
  */
 @RestController
-@RequestMapping(path = "/venta/resumen/{desde}/{hasta}")
+@RequestMapping(path = "pos/venta/resumen/{desde}/{hasta}")
 public class VentaResumenEndPoint extends DefaultEndpoint implements ResumenFacadeInterface {
 
     public static final String AUTORIZO_RESUMEN = "/autorizo";
@@ -39,40 +40,40 @@ public class VentaResumenEndPoint extends DefaultEndpoint implements ResumenFaca
     @GetMapping(AUTORIZO_RESUMEN)
     @Override
     public ResumenVentaWrapper<DayReviewWrapper<ProductovOrden>, ProductovOrden> getAutorizoResumen(
-            @PathVariable("desde") LocalDate desde,
-            @PathVariable("hasta") LocalDate hasta) {
+            @PathVariable("desde") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate desde,
+            @PathVariable("hasta") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate hasta) {
         return i.getAutorizoResumen(desde, hasta);
     }
 
     @GetMapping(COSTO_RESUMEN)
     @Override
     public ResumenVentaWrapper<DayReviewWrapper<InsumoRegistro>, InsumoRegistro> getCostoResumen(
-            @PathVariable("desde") LocalDate desde,
-            @PathVariable("hasta") LocalDate hasta) {
+            @PathVariable("desde") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate desde,
+            @PathVariable("hasta") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate hasta) {
         return i.getCostoResumen(desde, hasta);
     }
 
     @GetMapping(GASTO_RESUMEN)
     @Override
     public ResumenVentaWrapper<DayReviewWrapper<GastoVenta>, GastoVenta> getGastoResumen(
-            @PathVariable("desde") LocalDate desde,
-            @PathVariable("hasta") LocalDate hasta) {
+            @PathVariable("desde") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate desde,
+            @PathVariable("hasta") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate hasta) {
         return i.getGastoResumen(desde, hasta);
     }
 
     @GetMapping(SALARIO_RESUMEN)
     @Override
     public ResumenVentaWrapper<DayReviewWrapper<AsistenciaPersonal>, AsistenciaPersonal> getSalarioResumen(
-            @PathVariable("desde") LocalDate desde,
-            @PathVariable("hasta") LocalDate hasta) {
+            @PathVariable("desde") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate desde,
+            @PathVariable("hasta") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate hasta) {
         return i.getSalarioResumen(desde, hasta);
     }
 
     @GetMapping(VENTAS_RESUMEN)
     @Override
     public ResumenVentaWrapper<DayReviewWrapper<ProductovOrden>, ProductovOrden> getVentaResumen(
-            @PathVariable("desde") LocalDate desde,
-            @PathVariable("hasta") LocalDate hasta) {
+            @PathVariable("desde") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate desde,
+            @PathVariable("hasta") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate hasta) {
         return i.getVentaResumen(desde, hasta);
     }
 
